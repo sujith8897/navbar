851ce26f350ca6131c04a0ed29fd683a2eca04d2
sujith8897 <46921838+sujith8897@users.noreply.github.com>
2021-06-09 20:03:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#icon").click(function(){
+      $("ul").toggleClass("show");
+      $(".close-i").toggleClass("quit");
+    });
+
+    $("#close").click(function(){
+        $("ul").toggleClass("show");
+        $(".close-i").toggleClass("quit");
+    });
+
+  });
+
+
+
